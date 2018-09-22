aab495d186af68700a2d06fe3efda583644298ba
Jan <jan.spoerer@whu.edu>
2018-09-22 11:33:34 +0200

    Completes worst case time complexity calculation

--- time_complexity/worst-case-insertion-sort/worst-case-insertion-sort.py
+++ time_complexity/worst-case-insertion-sort/worst-case-insertion-sort.py
@@ -3,30 +3,40 @@ import time
 import matplotlib.pyplot as plt
 
 results = []
-decksizes = [10, 100, 1000, 10000]
+decksizes = [10, 100, 1000, 2000, 3000]
 repetition_limit = 10
 
-for n in range(0, len(decksizes)):
-    sum = 0
-    for repetition in range( 1, repetition_limit ):
-        state = list(reversed(range(decksizes[n])))
-
-        start = time.time()
-        j = 1
-        for j in range(j, len(state)):
-            key = state[j]
-            i = j - 1
+def sort_list( state, ascending = True ):
+    j = 1
+    for j in range( j, len(state) ):
+        key = state[j]
+        i = j - 1
+        if ascending:
             while i >= 0 and state[i] > key:
                 state[i + 1] = state[i]
                 i = i - 1
-            state[i + 1] = key
-        sum = sum + ( time.time() - start )
-        # print (state)
-        # print("operation took " + " {0:.9f}".format( time.time() - start ) + " seconds")
-    average = sum / repetition_limit
-    print(sum)
-    results.append ( average )
+        else:
+            while i >= 0 and state[i] < key:
+                state[i + 1] = state[i]
+                i = i - 1
+        state[i + 1] = key
+
+def track_time_complexity( decksizes, repetition_limit ):
+    for n in range(0, len( decksizes )):
+        sum = 0
+        for repetition in range( 1, repetition_limit ):
+            state = list( reversed( range( decksizes[n] ) ) )
+
+            start = time.time()
+            sort_list( state )
+            sum = sum + ( time.time() - start )
+        average = sum / repetition_limit
+        print( sum )
+        results.append ( average )
+    return results
+
+results = track_time_complexity( decksizes, repetition_limit )
 
 plt.plot( results )
-plt.ylabel('time complexity')
+plt.ylabel( 'time complexity' )
 plt.show()
