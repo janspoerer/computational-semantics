78298ab3875eff54ce0f1e411d64e91c9e921434
Jan <jan.spoerer@whu.edu>
2018-10-06 14:01:59 +0200

    Removes debugging print statements and fixes bug related to repeated values
    Before this fix, the find_pivot_index function crashed when there were repeated values in the list. The problem was fixed by making separate conditions that catch this case.

--- time_complexity/quick_sort/quick_sort.py
+++ time_complexity/quick_sort/quick_sort.py
@@ -4,10 +4,16 @@ def find_pivot_index(list):
     """
     if ((list[0] < list[1]) and (list[1] < list[2])) or ((list[2] < list[1]) and (list[1] < list[0])):
         return 1
-    if ((list[1] < list[0]) and (list[0] < list[2])) or ((list[2] < list[0]) and (list[0] < list[1])):
+    elif ((list[1] < list[0]) and (list[0] < list[2])) or ((list[2] < list[0]) and (list[0] < list[1])):
         return 0
-    if ((list[0] < list[2]) and (list[2] < list[1])) or ((list[1] < list[2]) and (list[2] < list[0])):
+    elif ((list[0] < list[2]) and (list[2] < list[1])) or ((list[1] < list[2]) and (list[2] < list[0])):
         return 2
+    elif list[0] == list[1]:
+        return 0
+    elif list[0] == list[2]:
+        return 0
+    elif list[1] == list[2]:
+        return 1
 
 def swap(list, first_index, second_index):
     temporary_value = list[first_index]
@@ -16,8 +22,7 @@ def swap(list, first_index, second_index):
     return list
 
 def quick_sort(list):
-    print("input", list)
-
+    print(list)
     # No border sorting needed if there are less than 3 elements
     if len(list) == 0:
         return []
@@ -35,8 +40,6 @@ def quick_sort(list):
 
     pivot_index = find_pivot_index(list)
 
-    print(list[pivot_index])
-
     # Swap pivot to first position
     list = swap(list, pivot_index, 0)
 
@@ -44,8 +47,6 @@ def quick_sort(list):
     border_index = 1
 
     while (border_index + border_offset) < (len(list)):
-        # print("border_index: ", border_index, "; border_offset: ", border_offset, "; 1")
-        # print(list)
         if list[border_index + border_offset] < list[0]:
             # Swap border and compared value
             list = swap(list, border_index, border_index + border_offset)
@@ -56,18 +57,12 @@ def quick_sort(list):
                 break
         else:
             border_offset = border_offset + 1
-        # print("border_index: ", border_index, "; border_offset: ", border_offset, "; 1")
-        print("Iteration: ", list)
-    print(list)
     if list[-1] < list[border_index]:
         list = swap(list, -1, border_index + 1)
         border_index = border_index + 1
-    print("After swap: ", list)
 
     # Swap border and pivot
     list = swap(list, border_index, 0)
-
-    print("components", list[0:border_index], list[border_index], list[border_index + 1:])
 
     left = []
     # Recursively call the function for the left side
@@ -76,7 +71,6 @@ def quick_sort(list):
         left = list[0:border_index]
     else:
         left = quick_sort(list[0:border_index])
-    print("output left", left)
 
     right = []
     # Recursively call the function for the right side
@@ -85,14 +79,21 @@ def quick_sort(list):
         right = list[border_index + 1:]
     else:
         right = quick_sort(list[border_index + 1:])
-    print("output right", right)
 
     result = []
     result.extend(left)
     result.append(list[border_index]) # The pivot value will be at this index because we earlier swapped the border and the pivot
     result.extend(right)
-    print("RESULT!!!!!: ", result)
     return result
 
-list = [7, 5, 3, 2, 6, 8, 9, 1]
+# list = [7, 5, 3, 2, 6, 8, 9, 1]
+# print("END RESULT", quick_sort(list))
+
+# list = [1, 2, 3, 4, 5, 6, 7, 8]
+# print("END RESULT", quick_sort(list))
+
+# list = [2, 5, 10, 22, 62, 84, 49, 61]
+# print("END RESULT", quick_sort(list))
+
+list = [2, 5, 10, 22, 62, 84, 49, 100, 1000, 12234, 2134, 2134, 4325, 3425, 457568, 23]
 print("END RESULT", quick_sort(list))
