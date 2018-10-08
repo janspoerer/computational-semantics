1ff7aa4c34bc5a0b97bbbf7bd79d7c646b0dfef2
Jan <jan.spoerer@whu.edu>
2018-10-08 20:58:27 +0200

    Error?

--- time_complexity/quick_sort/quick_sort.py
+++ time_complexity/quick_sort/quick_sort.py
@@ -1,19 +1,30 @@
+import random
+import time
+import matplotlib.pyplot as plt
+from tkinter import *
+
 def find_pivot_index(list):
     """
     Finds the median of the first 3 elements in a list
     """
-    if ((list[0] < list[1]) and (list[1] < list[2])) or ((list[2] < list[1]) and (list[1] < list[0])):
-        return 1
-    elif ((list[1] < list[0]) and (list[0] < list[2])) or ((list[2] < list[0]) and (list[0] < list[1])):
-        return 0
-    elif ((list[0] < list[2]) and (list[2] < list[1])) or ((list[1] < list[2]) and (list[2] < list[0])):
-        return 2
-    elif list[0] == list[1]:
-        return 0
-    elif list[0] == list[2]:
-        return 0
-    elif list[1] == list[2]:
-        return 1
+    low = 0
+    high = -1
+    mid = len(list) // 2
+
+    if (list[mid] <= list[low] <= list[high]):
+        result = low
+    elif (list[high] <= list[low] <= list[mid]):
+        result = low
+    elif (list[low] <= list[mid] <= list[high]):
+        result = mid
+    elif (list[high] <= list[mid] <= list[low]):
+        result = mid
+    elif (list[low] <= list[high] <= list[mid]):
+        result = high
+    elif (list[mid] <= list[high] <= list[low]):
+        result = high
+
+    return result
 
 def swap(list, first_index, second_index):
     """
@@ -28,7 +39,7 @@ def quick_sort(list):
     """
     Sorts a list of numbers in ascending order
     """
-    print(list)
+
     # No border sorting needed if there are less than 3 elements
     if len(list) == 0:
         return []
@@ -53,19 +64,22 @@ def quick_sort(list):
     border_index = 1
 
     while (border_index + border_offset) < (len(list)):
-        if list[border_index + border_offset] < list[0]:
+        if list[border_index + border_offset] <= list[0]:
             # Swap border and compared value
             list = swap(list, border_index, border_index + border_offset)
-
-            if (border_index + border_offset + 1) < (len(list)):
-                border_index = border_index + 1
-            else:
-                break
+            border_offset = 1
+            border_index = border_index + 1
         else:
             border_offset = border_offset + 1
-    if list[-1] < list[border_index]:
-        list = swap(list, -1, border_index + 1)
+
+    # Check if last value is smaller than the pivot and swap if necessary
+    if (list[-1] <= list[0]) and (border_index + 1 < len(list)):
+        swap(list, border_index + 1, -1)
         border_index = border_index + 1
+
+    # Check if border is larger than pivot and exclude it from the left list if yes
+    if list[border_index] > list[0]:
+        border_index = border_index - 1
 
     # Swap border and pivot
     list = swap(list, border_index, 0)
@@ -90,16 +104,24 @@ def quick_sort(list):
     result.extend(left)
     result.append(list[border_index]) # The pivot value will be at this index because we earlier swapped the border and the pivot
     result.extend(right)
+
     return result
 
-# list = [7, 5, 3, 2, 6, 8, 9, 1]
-# print("END RESULT", quick_sort(list))
+results = []
+decksizes = [10, 100, 1000, 10000]
+repetition_limit = 100
 
-# list = [1, 2, 3, 4, 5, 6, 7, 8]
-# print("END RESULT", quick_sort(list))
+for n in range(0, len(decksizes)):
+    sum = 0
+    for repetition in range( 1, repetition_limit ):
+        state = list(range( 1, decksizes[n] ))
+        random.shuffle( state )
+        start = time.time()
+        sum = sum + ( time.time() - start )
+    average = sum / repetition_limit
+    print(sum)
+    results.append ( average )
 
-# list = [2, 5, 10, 22, 62, 84, 49, 61]
-# print("END RESULT", quick_sort(list))
-
-list = [2, 5, 10, 22, 62, 84, 49, 100, 1000, 12234, 2134, 2134, 4325, 3425, 457568, 23]
-print("END RESULT", quick_sort(list))
+plt.plot( results )
+plt.ylabel('time complexity')
+plt.show()
