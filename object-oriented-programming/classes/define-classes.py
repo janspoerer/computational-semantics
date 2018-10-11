d64fbf14cc3d9932bae744558f862fc57e56c7ad
Jan <jan.spoerer@whu.edu>
2018-10-11 14:12:05 +0200

    Adds a phone class
    Phone class is used to store phone numbers and phone types in the Student class

--- object-oriented-programming/classes/define-classes.py
+++ object-oriented-programming/classes/define-classes.py
@@ -1,10 +1,18 @@
+class PhoneNumber:
+    def __init__(self, number, number_type):
+        self.number = number
+        self.number_type = number_type
+
 class Student:
-    def __init__(self, firstname, lastname, email="", grade=0, numbers={}):
+    def __init__(self, firstname, lastname, email="", grade=0, numbers=[]):
         self.firstname = firstname
         self.lastname = lastname
         self.email = email
         self.grade = grade
         self.numbers = numbers
+
+    def add_number(self, number_instance):
+        self.numbers.append(number_instance)
 
     def return_name(self):
         return self.firstname + " " + self.lastname
@@ -14,13 +22,13 @@ class Student:
 
     def __str__(self):
         result = "Name: " + self.return_name() + "\n" + "Email address: " + self.email + "\n"
-        for key, value in self.numbers.items():
-            result = result + str(key) + ": " + str(value) + "\n"
+        for value in self.numbers:
+            result = result + value.number_type + ": " + value.number + "\n"
         result
         return result
         # return "Home numbers: " + str(self.home_number).strip('[]') + "\n" + "Mobile numbers: " + str(self.mobile_number).strip('[]')
 
-jan = Student("Jan", "Spörer", "jan.spoerer@whu.edu", grade=1, numbers={"Home number": "12345", "Mobile number": "+491715395666", "Fax": "+491839240"})
+jan = Student("Jan", "Spörer", "jan.spoerer@whu.edu", 1, [PhoneNumber("+491715395666", "Mobile")])
 print(jan.firstname, jan.lastname)
 
 jan.lastname = "Spörer-Theron"
@@ -29,5 +37,9 @@ print(jan.firstname, jan.lastname)
 print(type(jan))
 
 print(jan.return_name())
+
+print(jan)
+
+jan.add_number(PhoneNumber("1234", "German"))
 
 print(jan)
