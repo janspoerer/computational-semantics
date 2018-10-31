98a8cc67d7cbf904efb68c5640f3650d60da64fb
Jan <jan.spoerer@whu.edu>
2018-10-31 17:26:18 +0100

    Cleanup

--- object-oriented-programming/binary-search-trees/binary-search-tree.py
+++ object-oriented-programming/binary-search-trees/binary-search-tree.py
@@ -1,9 +1,23 @@
+import math
+
 class Node:
     def __init__(self, name, age, child_left=None, child_right=None):
         self.name = name
         self.age = age
         self.child_left = child_left
         self.child_right = child_right
+
+    def __ge__(self, other):
+        return self.name >= other.name
+
+    def __le__(self, other):
+        return self.name <= other.name
+
+    def __gt__(self, other):
+        return self.name > other.name
+
+    def __eq__(self, other):
+        return self.name == other.name
 
     def get_instance(self, name):
         if self.name == name:
@@ -35,6 +49,48 @@ class Node:
             else:
                 self.child_right.insert(child)
 
+    def tree_to_list(root, unsorted_list=[]):
+        """
+        The root node does not necessarily have to be the actual node.
+        Subtrees can also be balanced by taking a root that is different from
+        the actual root.
+        """
+        if root.child_left is not None:
+            root.child_left.tree_to_list()
+            unsorted_list.append(root.child_left)
+        if root.child_right is not None:
+            root.child_right.tree_to_list()
+            unsorted_list.append(root.child_right)
+        return unsorted_list
+
+    def put_middle_to_front(sorted_sublist):
+        print("Chalo")
+        middle_element = sorted_sublist.pop(len(sorted_sublist) % 2)
+        left = sorted_sublist[:len(sorted_sublist) % 2]
+        right = sorted_sublist[len(sorted_sublist) % 2:]
+        if left is None or len(left) < 2:
+            left = Node.put_middle_to_front(left)
+        else:
+            left = []
+        if right is None or len(right) < 2:
+            right = Node.put_middle_to_front(right)
+        else:
+            right = []
+        result = []
+        result.append(middle_element)
+        print("MIDDLE ELEMENT!!!!!!!!!!!!!!:", middle_element)
+        print("LEFT ELEMENT!!!!!!!!!!!!!!:", left)
+        print("RIGHT ELEMENT!!!!!!!!!!!!!!:", right)
+        return result.extend(left).extend(right)
+
+    def rebalance_tree(self):
+        unsorted_list = self.tree_to_list()
+        unsorted_list.sort() # unsorted_list is now sorted
+        prepared_list = Node.put_middle_to_front(unsorted_list)
+        tree = prepared_list.pop(0)
+        for node in prepared_list:
+            tree.insert(node)
+        return tree
 
 head = Node("Jan", 1)
 head.insert(Node("Ana", "Ana age"))
@@ -47,10 +103,11 @@ head.insert(Node("Ruby", "Ruby age"))
 # print(head.child_right.name)
 # print(head.child_right.child_right.name)
 
-
 print(head.get_instance("Johannes").age)
 print(head.get_instance("Zakariya").age)
 print(head.get_instance("Jan").age)
 print(head.get_instance("Lenka").age)
 print(head.get_instance("Vahe").age)
 print(head.get_instance("Ruby").age)
+
+head.rebalance_tree()
