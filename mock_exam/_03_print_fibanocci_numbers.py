9dde989d70ad299b515c412fc6f8df46fa3d93d9
Jan Spörer <jan.spoerer@whu.edu>
2022-10-14 23:00:01 +0200

    All progress until the end of the last lecture

--- mock_exam/_03_print_fibanocci_numbers.py
+++ mock_exam/_03_print_fibanocci_numbers.py
@@ -4,11 +4,33 @@ Given a positive number, print out all Fibonacci numbers smaller or equal to tha
 For example, given the number 11 the program should print out: 1 1 2 3 5 8
 The next Fibonacci number would be 13 which is already larger than 11.
 """
+def print_fibonacci_below_or_equal(maximum_integer):
+    # We use a zero at the beginning because Wikipedia says so. Even though the assignment text says otherwise.
+    list_of_fibonacci_numbers = [0, 1]
+    
+    if maximum_integer > 0:
+        next_fibonacci = 1
+        while next_fibonacci <= maximum_integer:
+            # (list_of_fibonacci_numbers[-1] + list_of_fibonacci_numbers[-2]) > maximum_integer:
 
+            list_of_fibonacci_numbers.append(next_fibonacci)
+            next_fibonacci = list_of_fibonacci_numbers[-2] + next_fibonacci
+            """
+            if :
+                break
+            else:
+                list_of_fibonacci_numbers.append(list_of_fibonacci_numbers[-1] + list_of_fibonacci_numbers[-2])
+            """
 
+        print(list_of_fibonacci_numbers)
+    else:
+        print(f"Wrong input! Number is negative or zero: {maximum_integer}.")
 
+print_fibonacci_below_or_equal(11)
 
-
+# ["a", "b"]
+#   0    1   -> Coming from the left
+#  -2   -1   -> Coming from the right (reverse indexing)
 
 
 
--- mock_exam/_04_determine_if_sequence_is_increasing.py
+++ mock_exam/_04_determine_if_sequence_is_increasing.py
@@ -2,6 +2,23 @@
 Determine if a sequence of numbers is increasing
 Given a sequence of numbers, determine if each element is larger than the previous element
 """
-def is_increasing() -> bool:
+def is_ascending(list_of_numbers):
+    # Optional: Argument checking (should all be numbers)
+    sum(list_of_numbers) # n
+    
+    # Another argument check: List must at least have two elements
+    if len(list_of_numbers) < 2:
+        print("Problem. Number too small.")
+        return None
+    
+    i = 0
+    while i < (len(list_of_numbers) - 1): # n
+        print(f"i: {i}")
+        print(f"list_of_numbers: {list_of_numbers}")
+        if list_of_numbers[i] >= list_of_numbers[i+1]:
+            return False
+        i += 1 # i = i + 1
+    
     return True
-    return False
+
+is_ascending([1, 2, 2]) # -> False
